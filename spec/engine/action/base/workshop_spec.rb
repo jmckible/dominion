16ccb50ea7ea911ebbffa82d13db073245ded398
Jordan McKible <jordan@mckible.com>
2010-02-12 15:30:22 -0800

    Workshop spec.

--- spec/engine/action/base/workshop_spec.rb
+++ spec/engine/action/base/workshop_spec.rb
@@ -1,4 +1,17 @@
 require 'spec_helper'
 
 describe Workshop do
+  
+  it 'should execute' do
+    game = GameFactory.build
+    player = game.next_player
+    turn = Turn.new game, player
+    workshop = Workshop.new
+    copper = game.coppers.first
+    workshop.stub!(:select_card).and_return(copper)
+    turn.execute workshop
+    player.discard.first.should == copper
+    game.coppers.size.should == 31
+  end
+  
 end
