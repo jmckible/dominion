863b3a4a7bd2bb121738d6fff83827e126277738
Jordan McKible <jordan@mckible.com>
2010-02-12 00:10:49 -0800

    Implement Market.

--- lib/dominion/engine/action/base/market.rb
+++ lib/dominion/engine/action/base/market.rb
@@ -2,6 +2,14 @@ module Dominion
   module Engine
     class Market < Action
       def cost() 5 end
+        
+      def play(turn)
+        turn.draw_card
+        turn.add_action
+        turn.add_buy
+        turn.add_treasure 1
+      end
+        
     end
   end
 end
--- spec/engine/action/base/market_spec.rb
+++ spec/engine/action/base/market_spec.rb
@@ -1,4 +1,15 @@
 require 'spec_helper'
 
 describe Market do
+  
+  it 'should execute' do
+    game = GameFactory.build
+    turn = Turn.new game.next_player
+    turn.execute Market.new
+    turn.player.hand.size.should == 6
+    turn.number_actions.should == 2
+    turn.number_buys.should == 2
+    turn.treasure.should == 1
+  end
+  
 end
