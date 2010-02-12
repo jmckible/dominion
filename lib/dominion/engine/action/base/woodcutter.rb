7bc1fe132d8628c53fe338c513a0c8422830c46a
Jordan McKible <jordan@mckible.com>
2010-02-12 00:12:16 -0800

    Implement Woodcutter.

--- lib/dominion/engine/action/base/woodcutter.rb
+++ lib/dominion/engine/action/base/woodcutter.rb
@@ -2,6 +2,11 @@ module Dominion
   module Engine
     class Woodcutter < Action
       def cost() 3 end
+        
+      def play(turn)
+        turn.add_buy
+        turn.add_treasure 2
+      end
     end
   end
 end
--- spec/engine/action/base/woodcutter_spec.rb
+++ spec/engine/action/base/woodcutter_spec.rb
@@ -1,4 +1,13 @@
 require 'spec_helper'
 
 describe Woodcutter do
+  
+  it 'should execute' do
+    game = GameFactory.build
+    turn = Turn.new game.next_player
+    turn.execute Woodcutter.new
+    turn.number_buys.should == 2
+    turn.treasure.should == 2
+  end
+  
 end
