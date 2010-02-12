cee4a137d2d165bf2d792f7eedf00d024f8cef31
Jordan McKible <jordan@mckible.com>
2010-02-11 18:21:22 -0800

    Mine

---
+++ lib/dominion/engine/kingdom/base/mine.rb
@@ -0,0 +1,9 @@
+module Dominion
+  module Engine
+    class Mine < Kingdom
+      include Base
+      
+      def cost() 5 end
+    end
+  end
+end
---
+++ spec/engine/kingdom/base/mine_spec.rb
@@ -0,0 +1,4 @@
+require 'spec_helper'
+
+describe Mine do
+end
