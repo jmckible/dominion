ab1e05d745f7d40e09125a44bc22031cfcd7f6e3
Jordan McKible <jordan@mckible.com>
2010-02-15 23:08:46 -0800

    Specs for Highlander.

---
+++ spec/ai/highlander_spec.rb
@@ -0,0 +1,20 @@
+require 'spec_helper'
+
+describe Highlander do
+  before(:each) do
+    @highlander = Highlander.new 'Highlander'
+    @highlander.set_wishlist [Chancellor]
+  end
+  
+  it 'should select a Chancellor' do
+    chancellor = Chancellor.new
+    @highlander.select_buy([Copper.new, chancellor]).should == chancellor
+  end
+  
+  it 'should big money buy if action is set' do
+    @highlander.action = Smithy.new
+    province = Province.new
+    @highlander.select_buy([Copper.new, province]).should == province
+  end
+  
+end
