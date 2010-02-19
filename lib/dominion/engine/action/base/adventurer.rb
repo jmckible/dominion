0c51652e687ebdd8c1171e70795bf9f19d7ab884
Jordan McKible <jordan@mckible.com>
2010-02-18 19:09:01 -0800

    Implement Adventurer.

--- lib/dominion/engine/action/base/adventurer.rb
+++ lib/dominion/engine/action/base/adventurer.rb
@@ -1,8 +1,26 @@
 module Dominion
   module Engine
     class Adventurer < Action
-      def cost() 6 end
+      
+      def cost() 6            end
       def to_s() 'Adventurer' end
+        
+        
+      def play(turn)
+        treasure = []
+        player = turn.player
+        while player.can_draw? && treasure.size < 2
+          draw = player.draw.first
+          player.say "Drew a #{draw}"
+          if draw.is_a?(Treasure)
+            treasure << draw
+          else
+            turn.discard(draw)
+          end
+        end
+        treasure
+      end
+        
     end
   end
 end
--- spec/engine/action/base/adventurer_spec.rb
+++ spec/engine/action/base/adventurer_spec.rb
@@ -1,4 +1,24 @@
 require 'spec_helper'
 
 describe Adventurer do
+  
+  it 'should execute' do
+    game, player, turn = GameFactory.build
+    silver = Silver.new
+    gold = Gold.new
+    village = Village.new
+    smithy = Smithy.new
+    player.deck.unshift silver
+    player.deck.unshift village
+    player.deck.unshift smithy
+    player.deck.unshift gold
+    
+    turn.execute Adventurer.new
+    player.hand.size.should == 7
+    player.hand.should be_include(silver)
+    player.hand.should be_include(gold)
+    
+    player.discard.should == [village, smithy]
+  end
+  
 end
