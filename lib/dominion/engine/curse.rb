c19d3f2288068076074f1b9950dc0526c0869981
Jordan McKible <jordan@mckible.com>
2010-02-21 16:09:41 -0800

    Sorting for Curses.

--- lib/dominion/engine/curse.rb
+++ lib/dominion/engine/curse.rb
@@ -5,6 +5,10 @@ module Dominion
       def cost()             0  end
       def points(player=nil) -1 end
       def to_s() 'Curse'        end
+        
+      def <=>(other)
+        other.is_a?(Curse) ? 1 : -1
+      end
       
     end
   end
